e74728493a4716cbb372761148e97418c9771c39
harshalkatari9-debug <harshalkatari9@gmail.com>
2026-03-25 14:32:54 +0530

    UC2: Room modeling using abstraction and inheritance

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,19 +1,79 @@
 
+
+
+abstract class Room {
+    protected String type;
+    protected int beds;
+    protected double price;
+
+    public Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+
+    public abstract void displayDetails();
+}
+
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 1000);
+    }
+
+    public void displayDetails() {
+        System.out.println(type + " | Beds: " + beds + " | Price: ₹" + price);
+    }
+}
+
+
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 2000);
+    }
+
+    public void displayDetails() {
+        System.out.println(type + " | Beds: " + beds + " | Price: ₹" + price);
+    }
+}
+
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super("Suite Room", 3, 5000);
+    }
+
+    public void displayDetails() {
+        System.out.println(type + " | Beds: " + beds + " | Price: ₹" + price);
+    }
+}
+
+
 public class BookMyStayApp {
 
     public static void main(String[] args) {
 
+        System.out.println("===== BookMyStay Room Availability =====");
 
-        System.out.println("======================================");
-        System.out.println("     Welcome to Hotel Booking App     ");
-        System.out.println("======================================");
+        Room r1 = new SingleRoom();
+        Room r2 = new DoubleRoom();
+        Room r3 = new SuiteRoom();
 
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
 
-        System.out.println("Application Name : Hotel Booking System");
-        System.out.println("Version          : 1.0");
+        r1.displayDetails();
+        System.out.println("Available: " + singleAvailable);
+        System.out.println();
 
-        System.out.println("--------------------------------------");
-        System.out.println("Application Started Successfully");
-        System.out.println("--------------------------------------");
+        r2.displayDetails();
+        System.out.println("Available: " + doubleAvailable);
+        System.out.println();
+
+        r3.displayDetails();
+        System.out.println("Available: " + suiteAvailable);
     }
 }
